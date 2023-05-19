eb61930fc21996b1623eb2f1433363462b87d297
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-19 03:50:12 -0500

    carpeta de archivos del cliente

---
+++ pub/markdown.js
@@ -0,0 +1,13 @@
+
+/**
+ *  I - Listas los archivos Markdown disponibles
+ */
+
+/**
+ * II - Ver el contenido de un archivo Markdown traducido a HTML
+ */
+
+
+/**
+ * III - Crear nuevos archivos MarkDown y almacenarlos en el servidor
+ */
